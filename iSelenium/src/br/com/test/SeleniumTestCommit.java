bd748821cd912e28a36d6adc9a139a5ff56c0446
jonathanSoares <jonathanSoares@172.29.2.27>
2013-07-01 10:53:44 -0300

    Teste 02 para ver se foi para o github

--- iSelenium/src/br/com/test/SeleniumTestCommit.java
+++ iSelenium/src/br/com/test/SeleniumTestCommit.java
@@ -3,6 +3,6 @@ package br.com.test;
 public class SeleniumTestCommit {
 
 	public static void TestCommit(){
-		System.out.println("Test de commit");
+		System.out.println("Test de commit.. teste 02");
 	}
 }
