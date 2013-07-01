79b41a09ff42ea913f6e474f07041b8daa490ada
jonathanSoares <jonathanSoares@172.29.2.27>
2013-07-01 10:15:15 -0300

    Primeiro commit com o egit

---
+++ iSelenium/src/br/com/test/Selenium.java
@@ -0,0 +1,10 @@
+package br.com.test;
+
+public class Selenium {
+
+	public static void main(String[] args) {
+		System.out.println("Teste");
+
+	}
+
+}
---
+++ iSelenium/src/br/com/test/SeleniumTest.java
@@ -0,0 +1,15 @@
+package br.com.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SeleniumTest {
+
+	private static String text;
+	
+	@Test 
+	public static void Test(){
+		text = "teste";
+	}
+	
+}
